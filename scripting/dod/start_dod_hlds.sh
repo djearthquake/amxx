dabda5170671b40200efdbc686439a348baa7616
djearthquake <djearthquake@hotmail.com>
2021-04-06 10:25:23 -0500

    Create start_dod_hlds.sh

---
+++ scripting/dod/start_dod_hlds.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVICE="dlls/dod.so"
+if lsof | grep "$SERVICE" >/dev/null
+then
+echo "DoD found/ Not running"
+else
+
+SERVER_IP=`hostname  -I | cut -f1 -d' '`
+NAME=`hostname`
+ID=`whoami`
+#so you can crontab root niced at -20
+cd /home/$ID/Steam/steamapps/common/Half-Life/
+
+nohup screen -A -m -d -S dod ./hlds_run -tos -tcp -debug +condebug -game dod +map dod_caen +ip 0.0.0.0 +maxplayers 16 +mapchangecfgfile server.cfg +hostname $NAME$ID &
