032580768cf66aece8599e00f392b2ab5b5c587a
MegaBrutal <megabrutal+github@megabrutal.com>
2023-12-04 03:30:30 +0100

    Adjustments to hlds_fix.sh
    Get rid of absolute paths and do a more thorough ABI dependency checking.
    Also suggest packages to install if the required C/C++ libraries are missing.

--- hlds/hlds_fix.sh
+++ hlds/hlds_fix.sh
@@ -4,23 +4,53 @@
 #Error:./libstdc++.so.6: version `CXXABI_1.3.8' not found (required by /home/myuserid/Steam/steamapps/common/Half-Life/filesystem_stdio.so)
 #email debug.log to linux@valvesoftware.com
 
-if find -name "libstdc++.so.6"
+#Run this in the Half-Life root directory (where valve, gearbox, cstrike, etc. directories reside)
+
+TEST_LIB=filesystem_stdio.so
+
+test_ldd_local() {
+	LANG=C LD_LIBRARY_PATH=. ldd -v "$1"
+}
+
+#This function returns SUCCESS if it does find the problem!
+error_ldd_dependencies() {
+	#Usually we get error messages matching this pattern: "version \`(GCC|CXXABI)_[0-9.]*' not found",
+	#but matching for simply "not found" finds missing .so file too.
+	test_ldd_local "${TEST_LIB}" 2>&1 | grep -qP "not found"
+}
+
+if ! which ldd grep > /dev/null
 then
-unlink ./.local/share/Steam/steamcmd/linux32/libstdc++.so.6
-unlink ./Steam/steamapps/common/Half-Life/libstdc++.so.6
+	printf "Mandatory tools missing! Requirements: grep, ldd, which.\n"
+	exit 1
 fi
 
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/libstdc++.so.6:./.local/share/Steam/steamcmd/linux32/libstdc++.so.6
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/libstdc++.so.6:./Steam/steamapps/common/Half-Life/libstdc++.so.6
-
-if find -name "libgcc_s.so.1"
-
+if [ ! -f "${TEST_LIB}" ]
 then
-unlink ./Steam/steamapps/common/Half-Life/libgcc_s.so.1
+	printf "The %s file does not exist, ensure you are in the Half-Life root directory!\n" "${TEST_LIB}"
+	exit 1
 fi
 
-#in case it was not a link
-if ls ./Steam/steamapps/common/Half-Life/libgcc_s.so.1
+printf "Checking ABI dependencies of %s...\n" "${TEST_LIB}"
+test_ldd_local "${TEST_LIB}"
+
+#Check if installation is affected
+if ! error_ldd_dependencies
 then
-rm ./Steam/steamapps/common/Half-Life/libgcc_s.so.1
+	printf "\nThe library dependencies of %s file can be resolved successfully, no action required.\n" "${TEST_LIB}"
+	exit 0
+fi
+
+printf "\nTrying to remove local libstdc++.so.6 and libgcc_s.so.1...\n"
+rm -v libstdc++.so.6 libgcc_s.so.1
+
+if ! error_ldd_dependencies
+then
+	printf "\nFixed!\n"
+else
+	printf "\n%s\n%s\n%s\n\n" \
+		"Local installations of libstdc++.so.6 and libgcc_s.so.1 have been removed, but the dependencies of" \
+		"${TEST_LIB} still can't be resolved. You might need to install these packages:" \
+		"sudo apt install libc6:i386 libgcc-s1:i386 libstdc++6:i386"
+	exit 1
 fi
